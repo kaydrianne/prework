783e26e0c3c1aed0cddaf85bdfc9a439af5b56c6
TiAuna Dodd <tiaunadodd@TiAunas-MacBook-Pro.local>
2018-01-11 01:08:16 -0500

    Initial Commit

---
+++ Tipster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tipster
+//
+//  Created by TiAuna Dodd on 1/11/18.
+//  Copyright © 2018 TiAuna Dodd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
